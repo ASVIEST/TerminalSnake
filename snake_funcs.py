f7604eb6b9f81c1808f19818c5efb2a333417227
Anatoly Chistov <atchistov@gmail.com>
2020-10-23 20:09:49 +0300

    moving snake

--- snake_funcs.py
+++ snake_funcs.py
@@ -1,6 +1,8 @@
 from random import randint
 import time
 
+from kbhit import KBHit
+
 
 def field_to_text(width: int, height: int, snake: list, fruits: list) -> str:
     '''
@@ -8,7 +10,7 @@ def field_to_text(width: int, height: int, snake: list, fruits: list) -> str:
     snake: [(x, y), (x, y), ...]
     fruits: [(x, y), (x, y), ...]
     '''
-    
+
     field = [[' ' for x in range(width)] for y in range(height)]
     for x, y in snake:
         field[y][x] = 's'
@@ -21,17 +23,22 @@ def field_to_text(width: int, height: int, snake: list, fruits: list) -> str:
 
 
 def move_snake(snake, direction):
-    return snake
+    '''
+    direction = (0, 1)
+    '''
+    head = snake[0]
+    snake.pop()
+    snake.insert(0, (head[0] + direction[0], head[1] + direction[1]))
 
 
 def create_snake(width, height):
-    return [(randint(0, width - 1), randint(0, height - 1))]
+    return [(randint(0, width // 2), randint(0, height - 2))]
 
 
 def create_apple(width, height, fruits, snake):
-    apple = randint(0, width - 1), randint(0, height - 1)
+    apple = randint(0, width - 2), randint(0, height - 2)
     while apple in fruits or apple in snake:
-        apple = randint(0, width - 1), randint(0, height - 1)
+        apple = randint(0, width - 2), randint(0, height - 2)
     fruits.append(apple)
 
 
@@ -40,7 +47,8 @@ def check_lose(width, height, snake):
 
 
 def check_bounds(width, height, snake):
-    return False
+    head = snake[0]
+    return not (0 <= head[0] < width and 0 <= head[1] < height)
 
 
 def check_self(snake):
@@ -62,13 +70,13 @@ def enlarge_snake(snake, direction, amount=1):
 def update_screen(width, height, snake, fruits, direction):
     move_snake(snake, direction)
     if check_lose(width, height, snake):
-        game_over()
-        return
+        return False
     for apple in fruits:
         if check_food(snake, apple):
             enlarge_snake(snake, direction)
             break
     print(f'\033[{height + 2}A' + field_to_text(width, height, snake, fruits))
+    return True
 
 
 width = 60
@@ -77,12 +85,20 @@ height = 20
 fruits = []
 snake = create_snake(width, height)
 create_apple(width, height, fruits, snake)
-direction = 0
+direction = (1, 0)
 print('\n' * (height + 2))
 
+p = time.time()
+kb = KBHit()
 while True:
     c = time.time()
     if c - p < 0.1:
         time.sleep(0.1 - (c - p))
     p = time.time()
-    update_screen(width, height, snake, fruits, direction)
+    if kb.kbhit():
+        c = kb.getch()
+        if ord(c) == 27: # ESC
+            break
+        print(c)
+    # if not update_screen(width, height, snake, fruits, direction):
+    #     break
